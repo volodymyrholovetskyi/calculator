e60f87d8ed94415d881293f86a0fa3b4958bc2da
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2023-12-31 14:21:04 +0100

    define functions

--- app/src/main/java/ua/vholovetskyi/calculator/MainActivity.kt
+++ app/src/main/java/ua/vholovetskyi/calculator/MainActivity.kt
@@ -1,11 +1,42 @@
 package ua.vholovetskyi.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import net.objecthunter.exp4j.Expression
+import ua.vholovetskyi.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private var lastNumeric = false
+    private var stateError = false
+    private var lastDot = false
+    private lateinit var expression: Expression
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
     }
+
+    fun onAllClearClick(view: View) {
+    }
+
+    fun onEqualClick(view: View) {
+    }
+
+    fun onDigitClick(view: View) {
+    }
+
+    fun onOperatorClick(view: View) {
+    }
+
+    fun onBackClick(view: View) {
+    }
+
+    fun onClearClick(view: View) {
+    }
+
+
 }
