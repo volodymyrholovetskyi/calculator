a60c539d90b79ae23ebb8f5cd2fb7e04ca2336db
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-01-01 16:01:11 +0100

    Finish the project

--- app/src/main/java/ua/vholovetskyi/calculator/MainActivity.kt
+++ app/src/main/java/ua/vholovetskyi/calculator/MainActivity.kt
@@ -1,10 +1,14 @@
 package ua.vholovetskyi.calculator
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
 import ua.vholovetskyi.calculator.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,22 +25,75 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onAllClearClick(view: View) {
+        mainBinding.dataTextView.text = ""
+        mainBinding.resultTextView.text = ""
+        stateError = false
+        lastDot = false
+        lastNumeric = false
+        mainBinding.resultTextView.visibility = View.GONE
+
     }
 
     fun onEqualClick(view: View) {
+        onEqual()
+        mainBinding.dataTextView.text = mainBinding.resultTextView.text.toString().drop(1)
     }
 
     fun onDigitClick(view: View) {
+        if (stateError) {
+            mainBinding.dataTextView.text = (view as Button).text
+            stateError = false
+        } else {
+            mainBinding.dataTextView.append((view as Button).text)
+        }
+        lastNumeric = true
+        onEqual()
     }
 
     fun onOperatorClick(view: View) {
+        if (!stateError && lastNumeric) {
+            mainBinding.dataTextView.append((view as Button).text)
+            lastDot = false
+            lastNumeric = false
+            onEqual()
+        }
     }
 
     fun onBackClick(view: View) {
+        mainBinding.dataTextView.text = mainBinding.dataTextView.text.toString().dropLast(1)
+        try {
+            val lastChar = mainBinding.dataTextView.text.toString().last()
+            if (lastChar.isDigit()) {
+                onEqual()
+            }
+        } catch (e: Exception) {
+            mainBinding.resultTextView.text = ""
+            mainBinding.resultTextView.visibility = View.VISIBLE
+            Log.e("Last char error", e.toString())
+        }
     }
 
     fun onClearClick(view: View) {
+        mainBinding.dataTextView.text = ""
+        lastNumeric = false
     }
 
+    fun onEqual() {
+        if (lastNumeric && !stateError) {
+            val text = mainBinding.dataTextView.text.toString()
+            expression = ExpressionBuilder(text).build()
+            try {
+                val result = expression.evaluate()
+                mainBinding.resultTextView.visibility = View.VISIBLE
+                mainBinding.resultTextView.text = "=" + result.toString()
+
+            } catch (ex: ArithmeticException) {
+                Log.e("Evaluate Error", ex.toString())
+                mainBinding.resultTextView.text = "Error"
+                stateError = true
+                lastNumeric = false
+            }
+        }
+    }
 
 }
